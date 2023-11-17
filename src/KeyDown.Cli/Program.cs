165543160d8a8e697ec078e329ed97d2a29b4fda
Samdanae Imran <samdanae@gmail.com>
2023-11-18 03:04:13 +1300

    Rename folders

--- src/KeyDown.Cli/Program.cs
+++ src/KeyDown.Cli/Program.cs
@@ -1,32 +1,31 @@
 ﻿using ApplicationCore;
 using CommandLine;
 
-namespace KeyDown.Cli
-{
-    public class Program
-    {
-        public class Options
-        {
-            [Option('i', "input", Required = true, HelpText = "Specify input json file")]
-            public string? InputFile { get; set; }
-            
-            [Option('o', "output", Required = true, HelpText = "Specify output md file path")]
-            public string? OutputFile { get; set; }
-        }
+namespace KeyDown.Cli;
 
-        public static void Main(string[] args)
+public class Program
+{
+    public class Options
+    {
+        [Option('i', "input", Required = true, HelpText = "Specify input json file")]
+        public string? InputFile { get; set; }
+            
+        [Option('o', "output", Required = true, HelpText = "Specify output md file path")]
+        public string? OutputFile { get; set; }
+    }
+
+    public static void Main(string[] args)
+    {
+        Parser.Default.ParseArguments<Options>(args).WithParsed(o =>
         {
-            Parser.Default.ParseArguments<Options>(args).WithParsed(o =>
-                {
-                    Console.WriteLine($"Parsing {o.InputFile}...");
-                    var inputFileString = File.ReadAllText(o.InputFile ?? throw new InvalidOperationException());
-                    using var outputFileStream = File.AppendText(o.OutputFile ?? throw new InvalidOperationException());
+            Console.WriteLine($"Parsing {o.InputFile}...");
+            var inputFileString = File.ReadAllText(o.InputFile ?? throw new InvalidOperationException());
+            using var outputFileStream = File.AppendText(o.OutputFile ?? throw new InvalidOperationException());
                     
-                    IConversionService converter = new ConversionService();
-                    var markDown = converter.ConvertToMarkdown(inputFileString);
-                    outputFileStream.WriteAsync(markDown);
-                    Console.WriteLine($"Wrote output to {o.OutputFile}");
-                });
-        }
+            IConversionService converter = new ConversionService();
+            var markDown = converter.ConvertToMarkdown(inputFileString);
+            outputFileStream.WriteAsync(markDown);
+            Console.WriteLine($"Wrote output to {o.OutputFile}");
+        });
     }
 }
